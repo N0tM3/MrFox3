4b5e272eb11cffcdb2cc7ef4e138a16b24390b2e
Eduardo <ednvfr@gmail.com>
2017-03-07 17:04:21 +0100

    Comit 17:03

---
+++ app/src/main/java/com/spinblue/mrfox/Main.java
@@ -0,0 +1,18 @@
+package com.spinblue.mrfox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Eduardo on 07/03/2017.
+ * asdasd
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
